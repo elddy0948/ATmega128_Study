ce78f4fd2a4e881ffc3da3aec613d933fa0ca5a8
Hojoon Kim <elddy0948@gmail.com>
2023-11-05 23:45:53 +0900

    add uart project

---
+++ UART_Practice/UART_Practice/uart_communication.h
@@ -0,0 +1,20 @@
+﻿
+/*
+ * uart_communication.h
+ *
+ * Created: 2023-11-05 오후 11:22:16
+ *  Author: HJ KIM
+ */ 
+
+#define F_CPU 16000000L
+
+#include <avr/io.h>
+#include <util/delay.h>
+#define _BV(bit) (1 << (bit))
+
+typedef unsigned char u8;
+
+void UART1_init();
+void UART1_transmit(char data);
+u8 UART1_receive();
+int UART_main();
