e920fcc055af5a6924b79602a71feea0e22b5623
Joons <elddy0948@gmail.com>
2024-05-02 19:00:48 +0900

    feat bluetooth module

--- bluetooth_practice/bluetooth_practice/UART.c
+++ bluetooth_practice/bluetooth_practice/UART.c
@@ -1,11 +1,11 @@
 ﻿/*
-* UART1.c
+* UART.c
 *
 * Created: 2024-05-01 오후 10:14:04
 *  Author: HoJoon
 */
 
-#include "UART1.h"
+#include "UART.h"
 
 #include <avr/io.h>
 
@@ -19,6 +19,38 @@ void UART1_init(void)
 	
 	UCSR1B |= (1 << RXEN1);
 	UCSR1B |= (1 << TXEN1);
+}
+
+void UART0_init(void)
+{
+	UBRR0H = 0x00;
+	UBRR0L = 207;
+	
+	UCSR0A |= (1 << U2X0);
+	UCSR0C |= 0x06;
+	
+	UCSR0B |= (1 << RXEN0);
+	UCSR0B |= (1 << TXEN0);
+}
+
+void UART0_transmit(char data)
+{
+	while(!(UCSR0A & (1 << UDRE0)));
+	UDR0 = data;
+}
+
+unsigned char UART0_receive(void)
+{
+	while(!(UCSR0A & (1 << RXC0)));
+	return UDR0;
+}
+
+void UART0_print_string(char* str)
+{
+	for(int i = 0; str[i]; ++i)
+	{
+		UART0_transmit(str[i]);
+	}
 }
 
 void UART1_transmit(char data)
@@ -41,7 +73,7 @@ void UART1_print_string(char* str)
 	}
 }
 
-void UART1_print_1_byte_number(uint8_t n)
+void UART1_print_1_byte_number(unsigned char n)
 {
 	char numString[4] = "0";
 	int i, index = 0;
--- bluetooth_practice/bluetooth_practice/main.c
+++ bluetooth_practice/bluetooth_practice/main.c
@@ -9,29 +9,27 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#include "UART1.h"
+#include "UART.h"
 
 int main(void)
 {
+	UART0_init();
     UART1_init();
 	
 	_delay_ms(500);
 	
-    while (1) 
+	UART1_print_string("AT");
+	_delay_ms(1000);
+	UART1_print_string("AT+NAMEHotmega");
+	_delay_ms(1000);
+	UART1_print_string("AT+PIN1234");
+	_delay_ms(1000);
+	UART1_print_string("AT+BAUD4");
+	_delay_ms(1000);
+	
+    while (1)
     {
-		UART1_print_string("AT");
-		
-		_delay_ms(1000);
-		
-		UART1_print_string("AT+NAMEAtmega128Blue");
-		
-		_delay_ms(1000);
-		
-		UART1_print_string("AT+PIN1234");
-		
-		_delay_ms(1000);
-		
-		UART1_print_string("AT+BAUD4");
+		UART0_transmit(UART1_receive());
     }
 }
 
